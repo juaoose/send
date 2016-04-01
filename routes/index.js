e69c1f106562edcadc743fc05ef816405b5d3bf7
Jay <juaoose@users.noreply.github.com>
2016-03-31 20:34:57 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'Uniandes Cloud Week!',
-    color:  '#000000'
+    color:  '#ffffff'
   });
 });
 
